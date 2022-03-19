ef1b5d70139a257fbd4b8e0fd8b6a5313d35a924
sharathdt <sharath.dt@gmail.com>
2022-03-19 11:01:06 +0530

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+  import { request, gql } from "graphql-request";
+
+  const query = gql`
+    {
+      countries {
+        name
+        emoji
+      }
+    }
+  `;
+
+  export const load = async () => {
+    const data = await request("https://countries.trevorblades.com/", query);
+
+    return {
+      props: {
+        data,
+      },
+    };
+  };
+</script>
+
+<script>
+  export let data;
+</script>
+
+<!-- {JSON.stringify(data)} -->
+
+<h1>Countries</h1>
+
+{#each data.countries as { name, emoji }}
+  <p>{emoji} {name}</p>
+{/each}
